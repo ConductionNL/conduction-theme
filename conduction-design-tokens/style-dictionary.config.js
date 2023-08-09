7a636a27c5b520a98aa351753cdcd1c049c9bea9
Remko <remko@conduction.nl>
2023-08-09 10:40:02 +0200

    added font-size tokens

--- conduction-design-tokens/style-dictionary.config.js
+++ conduction-design-tokens/style-dictionary.config.js
@@ -1,5 +1,5 @@
-const config = require('./src/config.json');
-const { createConfig } = require('../../style-dictionary-config');
+const config = require("./src/config.json");
+const { createConfig } = require("../style-dictionary-config");
 
 module.exports = createConfig({
   selector: `.${config.prefix}-theme`,
