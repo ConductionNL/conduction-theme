5a6ccac08bfd62d64a7f0e3d9210f3be7a5029e2
Thijn <thijn@conduction.nl>
2024-09-16 16:41:26 +0200

    added font tokens

---
+++ municipalities/hof-van-twente-design-tokens/src/font.js
@@ -0,0 +1,10 @@
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/700-italic.css";
+import "@fontsource/roboto-condensed/300.css";
+import "@fontsource/roboto-condensed/300-italic.css";
+import "@fontsource/roboto-condensed/400.css";
+import "@fontsource/roboto-condensed/400-italic.css";
+import "@fontsource/roboto-condensed/700.css";
+import "@fontsource/roboto-condensed/700-italic.css";
